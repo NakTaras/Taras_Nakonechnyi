ad5f584783bc26823223947f91791eba80cc0a4c
NakTaras <51298366+NakTaras@users.noreply.github.com>
2021-11-10 19:56:49 +0200

    Complete homework

---
+++ src/main/java/com/epam/spring/homework1/pet/Pet.java
@@ -0,0 +1,18 @@
+package com.epam.spring.homework1.pet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class Pet {
+    @Autowired
+    private List<Animal> pets;
+
+    public void printPets() {
+        for (Animal pet : pets) {
+            System.out.println(pet.getAnimal());
+        }
+    }
+}
